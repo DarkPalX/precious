a77df675d952c3a3b55d4de3d299fdbd8331be6a
DarkPalX <raevinjhonpalx@gmail.com>
2024-04-17 11:30:29 +0800

    with shippinig coupons

--- resources/views/theme/layouts/banners/page-slider.blade.php
+++ resources/views/theme/layouts/banners/page-slider.blade.php
@@ -1,4 +1,4 @@
-<section id="slider" class="slick-carousel clearfix sub-banner">
+{{-- <section id="slider" class="slick-carousel clearfix sub-banner">
     <div id="page-title" class="bg-transparent border-0">
         <div class="container clearfix">
             <div class="title-head">
@@ -27,5 +27,40 @@
             </div>
         </div>
     </div>
-</section>
+</section> --}}
 
+<section id="slider" class="slick-carousel clearfix sub-banner">
+    <div class="banner-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12" style="padding:0;">
+					<div class="sub-banner-caption">
+						<div class="container" style="position: relative;">
+							<h2 class="text-center excerpt-1 text-light">{{ $page->name }}</h2>
+							<div class="sub-banner-flex">
+								@if(isset($breadcrumb))
+					            <ol class="breadcrumb nobottommargin flex-nowrap justify-content-center">
+					                @foreach($breadcrumb as $link => $url)
+					                    @if($loop->last)
+					                        <li class="breadcrumb-item active excerpt-1 text-light" aria-current="page">{{$link}}</li>
+					                    @else
+					                        <li class="breadcrumb-item text-nowrap"><a href="{{$url}}" class="text-light"><i class="icon-home"></i></a></li>
+					                    @endif
+					                @endforeach
+					            </ol>
+					            @endif
+							</div>
+						</div>
+					</div>
+                    <div id="banner" class="slick-slider">
+                        @foreach ($page->album->banners as $banner)
+                        <div class="hero-slide dark">
+                            <img src="{{ $banner->image_path }}" />
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
