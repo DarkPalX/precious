93b0bbce7f30406b6cf7c0517e648633df965d24
ariz0814 <137127368+ariz0814@users.noreply.github.com>
2024-04-29 11:12:06 +0800

    Update Library.php
    get subscribed read books list

--- app/Models/APIModels/Library.php
+++ app/Models/APIModels/Library.php
@@ -154,6 +154,141 @@ class Library extends Model
            
   }
 
+   public function getSubscribedReadBooksList($data){
+    
+    $UserID=$data['UserID'];
+    
+    $Status=$data['Status'];
+    $SearchText=$data['SearchText'];
+    
+    $Limit=$data['Limit'];
+    $PageNo=$data['PageNo'];
+    
+    $query = DB::table('read_books as rbooks')
+      ->join('products as prds', 'prds.id', '=', 'rbooks.product_id') 
+    
+       ->selectraw("
+          prds.id as book_ID,
+
+          COALESCE(prds.name,'') as name,
+          COALESCE(prds.author,'') as author,
+          COALESCE(prds.subtitle,'') as subtitle,
+          COALESCE(prds.short_description,'') as short_description,
+          
+          COALESCE(prds.slug,'') as slug,
+          COALESCE(prds.file_url,'') as file_url,          
+
+          COALESCE(prds.category_id,0) as category_id,
+          COALESCE(prds.book_type,'') as book_type,          
+      
+          COALESCE(prds.sku,'') as sku,          
+          COALESCE(prds.size,'') as size,
+          COALESCE(prds.weight,'') as weight,
+          COALESCE(prds.texture,'') as texture,
+          COALESCE(prds.uom,'') as uom,
+
+          COALESCE(prds.is_featured,0) as is_featured,
+          COALESCE(prds.is_best_seller,0) as is_best_seller,
+          COALESCE(prds.is_free,0) as is_free,
+          COALESCE(prds.is_premium,0) as is_premium,
+
+          COALESCE(prds.ebook_price,0) as price,   
+          COALESCE(prds.ebook_discount_price,0) as discount_price,      
+          
+          COALESCE(prds.reorder_point,0) as reorder_point,  
+
+          CONCAT(COALESCE(prds.name,''),' ', COALESCE(prds.author,''),'', COALESCE(prds.book_type,'') ,'', COALESCE(prds.subtitle,'')) as search_fields,  
+
+          COALESCE((
+               SELECT 
+                  prod_img.path FROM 
+                      product_photos as prod_img                  
+                  LEFT JOIN products as prods ON prods.id = prod_img.product_id
+                      WHERE prod_img.product_id = rbooks.product_id
+                      AND prod_img.is_primary = 1    
+                  LIMIT 1                                
+              )
+        ,'') as image_path,
+
+        COALESCE((
+               SELECT ROUND(avg(rating))
+                  FROM product_reviews as rev
+                WHERE rev.product_id = prds.id     
+                AND rev.status = 1 
+             LIMIT 1                                
+              )
+        ,0) as rating,
+
+        COALESCE((
+               SELECT 
+                  promo.discount FROM 
+                        promos as promo                  
+                  INNER JOIN promo_products as promo_prods ON promo_prods.promo_id = promo.id  
+                       WHERE promo_prods.product_id = rbooks.product_id                        
+                       AND promo_prods.deleted_at IS NULL                     
+                  LIMIT 1                                
+              )
+        ,0) as promo_discount_percent,
+
+
+        COALESCE((
+               SELECT 
+                   (prds.ebook_price - (promo.discount/100 * prds.ebook_price)) FROM 
+                        promos as promo                  
+                  INNER JOIN promo_products as promo_prods ON promo_prods.promo_id = promo.id  
+                       WHERE promo_prods.product_id = rbooks.product_id                        
+                       AND promo_prods.deleted_at IS NULL                     
+                  LIMIT 1                                
+              )
+        ,0) as promo_discount_price,
+
+          COALESCE(prds.status,'') as status          
+          
+        ");    
+
+    $query->where("rbooks.user_id",'=',$UserID);    
+    $query->where("rbooks.is_subscribed",'=',1);    
+    $query->where("rbooks.deleted_at",'=',null);   
+   
+      // if($Status!='' && $Status!='All'){
+
+      //   if($Status=='Epub'){
+      //     $query->where("prds.file_url","!=",null);    
+      //   }
+
+      //   if($Status=='Physical'){
+      //     $query->where("prds.file_url","==",null);    
+      //   } 
+
+      // }    
+                                  
+      if($SearchText != ''){
+        $arSearchText = explode(" ",$SearchText);
+        if(count($arSearchText) > 0){
+            for($x=0; $x< count($arSearchText); $x++) {
+                $query->whereraw(
+                    "CONCAT_WS(' ',
+                        COALESCE(prds.name,''),
+                        COALESCE(prds.author,''),                        
+                        COALESCE(prds.subtitle,'')
+                    ) like '%".str_replace("'", "''", $arSearchText[$x])."%'");
+             }
+        }
+    }
+
+    if($Limit > 0){
+      $query->limit($Limit);
+      $query->offset(($PageNo-1) * $Limit);
+    }
+
+    $query->orderBy("prds.name","ASC");    
+    $list = $query->get();
+                             
+     return $list;             
+           
+  }
+
+
  public function checkProductsIfExistInLibrary($ProductID,$CustomerID){
       
     $IsExist = false; 
@@ -171,5 +306,44 @@ class Library extends Model
     
     return $IsExist;
   }
+
+  public function saveReadBooks($data){
+
+    $TODAY = date("Y-m-d H:i:s");
+    $PaymentDate = date("Y-m-d");
+    
+    $UserID=$data['UserID'];    
+    $ProductID=$data['ProductID'];    
+    $IsSubscribe=$data['IsSubscribe'];    
+    
+    $ReadBookID = DB::table('read_books')
+        ->insertGetId([                                            
+          'user_id' => $UserID,              
+          'product_id' => $ProductID,                                            
+          'is_subscribed' => $IsSubscribe,                                                      
+          'created_at' => $TODAY             
+        ]);
+
+  }
+
+   public function checkProductsIfExistInSubscribedBooks($ProductID,$CustomerID){
+      
+    $IsExist = false; 
+    
+    $list = DB::table('read_books')          
+        ->whereRaw('user_id=?',[$CustomerID])    
+        ->whereRaw('product_id=?',[$ProductID])  
+        ->where('is_subscribed',"=",1)                                      
+        ->get();
+
+    if(count($list)>0){
+        $IsExist=true;
+    }else{
+        $IsExist=false;
+    }
+    
+    return $IsExist;
+  }
+
 
 }
