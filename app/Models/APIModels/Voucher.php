bc09598e198ef4b40d297db2ba869c2d330aa217
ariz0814 <137127368+ariz0814@users.noreply.github.com>
2024-05-15 06:15:23 +0800

    Update Voucher.php
    filter ebooks only

--- app/Models/APIModels/Voucher.php
+++ app/Models/APIModels/Voucher.php
@@ -59,7 +59,7 @@ class Voucher extends Model
       ->where("coup.location","=",null)                     
       ->where("coup.deleted_at","=",null)
       ->where("coup.activation_type","!=",'manual');
-      ->where("coup.activation_type","!=",'phyiscal');
+      ->where("coup.applicable_product_type","!=",'phyiscal');
       
     //   ->orWhereRaw("CONCAT('|',coup.scope_customer_id,'|') LIKE CONCAT('%|',". $SearchText.",'|%')");
 
