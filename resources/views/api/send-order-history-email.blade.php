217047e90d081be49e3bc197ea3eb921949a29b6
ariz0814 <137127368+ariz0814@users.noreply.github.com>
2024-05-17 08:29:22 +0800

    Update send-order-history-email.blade.php
    update email format

--- resources/views/api/send-order-history-email.blade.php
+++ resources/views/api/send-order-history-email.blade.php
@@ -68,45 +68,12 @@
         }
     </style>
 
+    @php($SubTotal=0)
+
 <body style="margin: 0 !important; padding: 0 !important; background-color: #eeeeee;" bgcolor="#eeeeee">
     <table border="0" cellpadding="0" cellspacing="0" width="100%">
         
-        @php($ReferenceNo='')
-        @php($ModeOfPayment='')
-        @php($OrderDate='')
-
-        @php($FullName='')
-        @php($MobileNo='')
-        @php($EmailAddress='')
-
-        @php($Address='')      
-     
-        @php($EWalletPayment=0)  
-        @php($DiscountAmount=0)
-
-        @php($SubTotal=0)
-        @php($GrandTotal=0)
-
-        @php($OrderInstruction='')  
-
-        @If(isset($OrderInfo) > 0)  
-            @php($ReferenceNo=$OrderInfo->order_number)  
-            @php($ModeOfPayment=$OrderInfo->payment_method)
-            @php($OrderDate=date_format(date_create($OrderInfo->order_date),'F d, Y H:i A'))
-                         
-            @php($FullName=$OrderInfo->customer_name)
-                    
-            @php($MobileNo=$OrderInfo->customer_contact_number)       
-            @php($EmailAddress=$OrderInfo->customer_email)        
-            
-            @php($Address=$OrderInfo->customer_delivery_adress)      
-                        
-            @php($DiscountAmount=$OrderInfo->discount_amount)                
-            @php($EWalletPayment=$OrderInfo->ecredit_amount)
-            
-                
-        @endif
-
+      
         <tr>
             <td align="center" style="background-color: #eeeeee;" bgcolor="#eeeeee">
                 <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:1000px;">
@@ -115,7 +82,7 @@
                             <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:1000px;">
                                 <tr>
                                     <td align="center" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding-top: 25px;"> <img src="https://img.icons8.com/carbon-copy/100/000000/checked-checkbox.png" width="125" height="120" style="display: block; border: 0px;" /><br>
-                                        <h2 style="font-size: 30px; font-weight: 800; line-height: 36px; color: #333333; margin: 0;"> Thank You For Your Order! </h2>
+                                        <h2 style="font-size: 30px; font-weight: 800; line-height: 36px; color: #333333; margin: 0;"> Purchase Order List </h2>
                                     </td>
                                 </tr>
                                 <tr>
@@ -129,47 +96,23 @@
                                     </td>
                                 </tr>
 
-                                <!-- ORDER DETAILS -->
+               
                                 <tr>
                                     <td align="left" style="padding-top: 20px;">
                                         <table cellspacing="0" cellpadding="0" border="0" width="100%">
                                             <tr>
-                                                <td width="60%" align="left" bgcolor="#000000" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;color:#fff;"> {{$ReferenceNo}} </td>
-                                                <td width="40%" align="right" bgcolor="#000000" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;color:#fff;"> {{$OrderDate}} </td>
-                                            </tr>
-                                            <tr>
-                                                <td width="60%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 15px 10px 5px 10px;">{{$FullName}} </td>
-                                            </tr>
-                                            <tr>
-                                                <td width="60%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 50px 10px 0px 10px;"> {{$EmailAddress}} </td>
-                                            </tr>
-                                            <tr>
-                                                <td width="60%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 5px 10px;"> {{$MobileNo}}</td>
-                                            </tr>
-                                            <tr>
-                                                <td width="60%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 30px 10px 0px 10px;">  {{$Address}}</td>
-                                            </tr>
-                                        </table>
-                                    </td>
-                                </tr>
+                                                <td width="20%" align="left" bgcolor="#000000" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;color:#fff;"> Order No </td>
 
+                                                <td width="20%" align="right" bgcolor="#3E91F6" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;color:#fff;"> Order Date </td>
 
-                                <!-- ITEMS -->
-                                <tr>
-                                    <td align="left" style="padding-top: 20px;">
-                                        <table cellspacing="0" cellpadding="0" border="0" width="100%">
-                                            <tr>
                                                 <td width="30%" align="left" bgcolor="#3E91F6" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;color:#fff;"> Product </td>
                                                 
                                                 <td width="20%" align="center" bgcolor="#3E91F6" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;color:#fff;"> Unit Price </td>
                                                 
-                                                <td width="20%" align="center" bgcolor="#3E91F6" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;color:#fff;"> Discounted Price </td>
+                                                <td width="20%" align="center" bgcolor="#3E91F6" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;color:#fff;"> Disc. Price </td>
 
                                                 <td width="10%" align="center" bgcolor="#3E91F6" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;color:#fff;"> Quantity </td>
-                                                                                                
-                                                <td width="60%" align="left" bgcolor="#000000" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;color:#fff;"> {{$ReferenceNo}} </td>
-                                                <td width="40%" align="right" bgcolor="#000000" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;color:#fff;"> {{$OrderDate}} </td>
-
+                                                                                                                                                
                                             </tr>
                                                                                         
                                             @foreach($OrderItemList as $order_item)
@@ -189,6 +132,10 @@
                                             
 
                                             <tr>
+                                                <td width="20%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 15px 10px 5px 10px;">{{$order_item->order_number}}</td>
+
+                                                <td width="20%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 15px 10px 5px 10px;">{{$order_item->order_date_format}}</td>
+
                                                 <td width="30%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 15px 10px 5px 10px;">{{$ProductName}}</td>
                                                 
                                                 <td width="20%" align="right" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 15px 10px 5px 10px;">₱{{number_format($Price,2)}}</td>
@@ -205,23 +152,6 @@
                                         </table>
                                     </td>
                                 </tr>
-
-                                <!-- SUBTOTAL -->
-                       <!--          <tr>
-                                    <td align="left" style="padding-top: 20px;">
-                                        <table cellspacing="0" cellpadding="0" border="0" width="100%">
-                                            <tr>
-                                                <td colspan="3" width="75%" align="right" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px; border-top: 3px solid #eeeeee; border-bottom: 3px solid #eeeeee;"> SUBTOTAL: </td>
-                                                <td width="25%" align="right" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px; border-top: 3px solid #eeeeee; border-bottom: 3px solid #eeeeee;"> ₱{{number_format($SubTotal,2)}} </td>
-                                            </tr>
-                                                                          
-                                            <tr>
-                                                <td colspan="3" width="75%" align="right" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px; border-top: 3px solid #eeeeee; border-bottom: 3px solid #eeeeee;"> GRAND TOTAL: </td>
-                                                <td width="25%" align="right" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px; border-top: 3px solid #eeeeee; border-bottom: 3px solid #eeeeee;"> ₱{{number_format($SubTotal - $DiscountAmount - $EWalletPayment,2)}} </td>
-                                            </tr>
-                                        </table>
-                                    </td>
-                                </tr> -->
 
                                 <tr>
                                     <td align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding-top: 10px;">
