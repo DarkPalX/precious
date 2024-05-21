27285af11f5b2587ba21b6f9ceca75944fb66b81
ariz0814 <137127368+ariz0814@users.noreply.github.com>
2024-05-21 15:11:26 +0800

    Update member-registration.blade.php
    fix

--- resources/views/api/member-registration.blade.php
+++ resources/views/api/member-registration.blade.php
@@ -77,7 +77,13 @@
             <td  style="padding-top:40px;padding-bottom:0px;padding-left:50px;padding-right:50px;font-family:sans-serif;font-size:12px;line-height:20px;color:#232323;text-align:left;font-weight:normal">
                Dear <b>{{ $FullName }}</b>,
               <br><br>
-                  Welcome to PRECIOUS PAGES CORP and thank you for your interest for signing up. Please login your account now and update your profile information now.
+                  Welcome to   {{ config("app.CompanyName") }} and thank you for your interest for signing and becoming one of our customer. Please login your account now and and complete the registration process, please enter this new 4 digit verification code below:
+
+                   <br><br>
+                  
+                  CODE: <b>{{ $VerificationCode }}</b> 
+
+                  <br><br>
 
             </td> 
             </tr> 
