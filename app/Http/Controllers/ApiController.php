7505f6e1192389abfaeb05fa868ed4af35f60455
ariz-kate <kate_paran@yahoo.com>
2024-05-27 11:17:17 +0800

    Update ApiController.php
    test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -991,10 +991,12 @@ public function saveBookMarks(Request $request){
 
 public function updateBookMarks(Request $request){
 
-   return response([
-            'response' => 'Success',
-            'message' => 'Update To Book Mark is working...'
-     ], 200);
+   // return response([
+   //          'response' => 'Success',
+   //          'message' => 'Update To Book Mark is working...'
+   //   ], 200);
+
+  dd("here");
 
 
     $Misc = new Misc();
