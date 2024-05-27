54ad7dfb3eadea3d690c694359474490391105d8
ariz-kate <kate_paran@yahoo.com>
2024-05-27 11:41:30 +0800

    Update epub_viewer.blade.php
    test

--- resources/views/api/epub_viewer.blade.php
+++ resources/views/api/epub_viewer.blade.php
@@ -378,7 +378,7 @@
         book.package.metadata.direction === "rtl" ? rendition.prev() : rendition.next();
 
        var $select = document.getElementById("toc");
-       index=$select.selectedIndex; 
+       index=$select.selectedIndex+1; 
 
         var $ChapterNo = document.getElementById("BookMarkIndex");
         $ChapterNo.value=index;  
@@ -391,7 +391,7 @@
         book.package.metadata.direction === "rtl" ? rendition.next() : rendition.prev();    
        
        var $select = document.getElementById("toc");
-       index=$select.selectedIndex; 
+       index=$select.selectedIndex+1; 
 
        var $ChapterNo = document.getElementById("BookMarkIndex");
         $ChapterNo.value=index;     
@@ -496,7 +496,7 @@
           rendition.display(url);
 
            var $ChapterNo = document.getElementById("BookMarkIndex");
-           $ChapterNo.value=index;   
+           $ChapterNo.value=index+1;   
           
           return false;
       };
