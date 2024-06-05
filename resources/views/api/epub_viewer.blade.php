a4459f7654fc23aa00b0df10e071fc7aa70ebb33
ariz-kate <kate_paran@yahoo.com>
2024-06-05 15:04:50 +0800

    Update epub_viewer.blade.php
    fix

--- resources/views/api/epub_viewer.blade.php
+++ resources/views/api/epub_viewer.blade.php
@@ -2491,45 +2491,294 @@
 
        if(background=='light'){
 
-          rendition.themes.register("custom_font_times", {
-              body: {     
-                "background-color": "white !important",                        
-                "font-family": "Times New Roman, Times, serif !important",
-              },
-              "p": {                   
-                "color": "black !important",
-              },
-          });
+
+        if(font_size=="10"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "white !important",                        
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "10px !important",
+                },
+                "p": {                   
+                  "color": "black !important",
+                },
+            });    
+          }
+
+          if(font_size=="11"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "white !important",                        
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "11px !important",
+                },
+                "p": {                   
+                  "color": "black !important",
+                },
+            });    
+          }
+
+          if(font_size=="12"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "white !important",                        
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "11px !important",
+                },
+                "p": {                   
+                  "color": "black !important",
+                },
+            });    
+          }
+
+          if(font_size=="13"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "white !important",                        
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "13px !important",
+                },
+                "p": {                   
+                  "color": "black !important",
+                },
+            });    
+          }
+
+          if(font_size=="15"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "white !important",                        
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "15px !important",
+                },
+                "p": {                   
+                  "color": "black !important",
+                },
+            });    
+          }
+
+
+          if(font_size=="18"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "white !important",                        
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "18px !important",
+                },
+                "p": {                   
+                  "color": "black !important",
+                },
+            });    
+          }
+
+
+          if(font_size=="20"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "white !important",                        
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "20px !important",
+                },
+                "p": {                   
+                  "color": "black !important",
+                },
+            });    
+          }
+
 
          rendition.themes.select("custom_font_times");                   
       }
 
       if(background=='dark'){
 
-          rendition.themes.register("custom_font_times", {
-              body: {     
-                "background-color": "black !important",                        
-                "font-family": "Times New Roman, Times, serif !important",
-              },
-              "p": {                   
-                "color": "white !important",
-              },
-          });
+           if(font_size=="10"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "black !important",                        
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "10px !important",
+                },
+                "p": {                   
+                  "color": "white !important",
+                },
+            });    
+          }
+
+          if(font_size=="11"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "black !important",                        
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "11px !important",
+                },
+                "p": {                   
+                  "color": "white !important",
+                },
+            });    
+          }
+
+          if(font_size=="12"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "black !important",                        
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "12px !important",
+                },
+                "p": {                   
+                  "color": "white !important",
+                },
+            });    
+          }
+
+
+          if(font_size=="13"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "black !important",                        
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "13px !important",
+                },
+                "p": {                   
+                  "color": "white !important",
+                },
+            });    
+          }
+
+          if(font_size=="15"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "black !important",                        
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "15px !important",
+                },
+                "p": {                   
+                  "color": "white !important",
+                },
+            });    
+          }
+
+          if(font_size=="18"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "black !important",                        
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "18px !important",
+                },
+                "p": {                   
+                  "color": "white !important",
+                },
+            });    
+          }
+
+          if(font_size=="20"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "black !important",                        
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "20px !important",
+                },
+                "p": {                   
+                  "color": "white !important",
+                },
+            });    
+          }
 
          rendition.themes.select("custom_font_times");                   
       }
 
       if(background=='septia'){
 
-          rendition.themes.register("custom_font_times", {
-              body: {     
-                "background-color": "#f4eacd !important",
-                "font-family": "Times New Roman, Times, serif !important",
-              },
-              "p": {                   
-                "color": "#704214 !important",
-              },
-          });
+            if(font_size=="10"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "#f4eacd !important",
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "10px !important",
+                },
+                "p": {                   
+                  "color": "#704214 !important",
+                },
+            });    
+          }
+
+           if(font_size=="11"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "#f4eacd !important",
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "11px !important",
+                },
+                "p": {                   
+                  "color": "#704214 !important",
+                },
+            });    
+          }
+
+           if(font_size=="12"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "#f4eacd !important",
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "12px !important",
+                },
+                "p": {                   
+                  "color": "#704214 !important",
+                },
+            });    
+          }
+
+          if(font_size=="13"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "#f4eacd !important",
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "13px !important",
+                },
+                "p": {                   
+                  "color": "#704214 !important",
+                },
+            });    
+          }
+
+          if(font_size=="15"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "#f4eacd !important",
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "15px !important",
+                },
+                "p": {                   
+                  "color": "#704214 !important",
+                },
+            });    
+          }
+
+          if(font_size=="18"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "#f4eacd !important",
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "18px !important",
+                },
+                "p": {                   
+                  "color": "#704214 !important",
+                },
+            });    
+          }
+
+          if(font_size=="20"){
+                rendition.themes.register("custom_font_times", {
+                body: {     
+                  "background-color": "#f4eacd !important",
+                  "font-family": "Times New Roman, Times, serif !important",
+                  "font-size": "20px !important",
+                },
+                "p": {                   
+                  "color": "#704214 !important",
+                },
+            });    
+          }
+
 
          rendition.themes.select("custom_font_times");                          
        } 
