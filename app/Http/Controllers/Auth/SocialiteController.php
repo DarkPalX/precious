ba4457daab1cb0ef6d343aaf10b618e69d70912f
DarkPalX <raevinjhonpalx@gmail.com>
2024-07-03 08:28:30 +0800

    fb login fixing

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class SocialiteController extends Controller
 {
