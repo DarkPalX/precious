fada131357d8bcd21d03e5f652b0060924d38966
DarkPalX <raevinjhonpalx@gmail.com>
2024-07-05 16:53:53 +0800

    with freshchat

--- resources/views/theme/main.blade.php
+++ resources/views/theme/main.blade.php
@@ -7,6 +7,24 @@
 <script
 	src='//fw-cdn.com/11794505/4403633.js'
 	chat='true'>
+
+	// Copy the below lines under window.fcWidget.init inside fw_chat_widget function in the above snippet
+
+	// To set unique user id in your system when it is available
+	window.fcWidget.setExternalId("john.doe1987");
+
+	// To set user name
+	window.fcWidget.user.setFirstName("John");
+
+	// To set user email
+	window.fcWidget.user.setEmail("john.doe@gmail.com");
+
+	// To set user properties
+	// Note that any other fields passed as properties which are not in the CRM Contact properties already, they will be ignored.
+	window.fcWidget.user.setProperties({
+		cf_plan: "Pro",                 // meta property 1
+		cf_status: "Active"             // meta property 2
+	});
 </script>
 
 @include('theme.layouts.components.styles')
