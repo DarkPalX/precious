c5147129722a3f80916a33a7a357fee359ff2d82
ariz-kate <kate_paran@yahoo.com>
2024-07-25 13:59:51 +0800

    Update subscribe-email.blade.php
    update email subscribed

--- resources/views/api/subscribe-email.blade.php
+++ resources/views/api/subscribe-email.blade.php
@@ -1,68 +1,161 @@
-<!DOCTYPE HTML>
-<html>
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<div bgcolor="#fafafa">
+  <div style="background-color:#fafafa"> 
+  <table role="presentation" width="100%" bgcolor="#fafafa" cellpadding="0" cellspacing="0" border="0" align="center" style="margin:0px auto;width:100%!important;min-width:100%!important"> 
+    <tbody>
+      <tr> 
+        <td> 
+          <table role="presentation" cellpadding="0" cellspacing="0" border="0" width="600" align="center" style="margin:0px auto;width:600px"> 
+            <tbody>
+            <tr> 
+            <td align="center" valign="top">
+            <table role="presentation" bgcolor="#ffffff" width="100%" cellpadding="0" cellspacing="0" border="0" align="center"   style="margin:0px auto;width:100%!important;min-width:100%!important"> 
+              <tbody></tbody>  
+            </table>
+            <table role="presentation" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
+            <tbody> 
+            <tr> 
+              <td dir="rtl" style="text-align:center;font-size:0;background:#2e3c5f"> 
+              <div  bgcolor="#ffffff" style="max-width:200px;width:100%;display:inline-block;vertical-align:top" dir="ltr"> 
+              <table bgcolor="#2e3c5f" width="100%" bgcolor="#ffffff" valign="top" border="0" cellpadding="0" cellspacing="0" style="border-collapse:collapse"> 
+                  <tbody> 
+                    <tr> 
+                    <td style="line-height:1px"> 
+                      <table role="presentation" width="250" height="140" align="center" cellpadding="0" cellspacing="0" border="0"  style="width:250px;height:140px">
+                      <tbody> 
+                      <tr> 
+                        <td height="140" valign="bottom" style="text-align:left;height:140px"> 
+                          <div style="display:inline-block">                              
+                            <img src="https://beta.ebooklat.phr.com.ph/storage/logos/ebooklat-logo-horizontal.png" style="width: 106%;">                      
+                          </div> 
+                      </td> 
+                      </tr> 
+                      </tbody> 
+                      </table> 
+                     </td> 
+                    </tr> 
+                  </tbody> 
+                </table> 
+              </div> 
+               
+              <div  bgcolor="#677b8c" style="max-width:300px;width:100%;display:inline-block;vertical-align:top" dir="ltr"> 
+                <table width="100%" bgcolor="#677b8c" align="left" valign="top" border="0" cellpadding="0" cellspacing="0" style="border-collapse:collapse"> 
+                  <tbody> 
+                    <tr> 
+                    <td valign="middle"> 
+                    <table role="presentation" width="450" height="140" align="center" cellpadding="0" cellspacing="0" border="0"  style="width:450px;height:140px"> 
+                    <tbody> 
+                    <tr> 
+                    <td  bgcolor="#2e3c5f" valign="middle" height="140" style="height:140px" > 
+                    <table role="presentation" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
+                    <tbody> 
+                    <tr> 
+                    <td  valign="top" style="padding-left:50px;padding-right:35px;font-family:sans-serif;font-size:24px;line-height:32px;color:#ffffff;text-align:left;font-weight:normal">
+            
+                          <br>
+                          <br>
+                             Email Subscribed 
+                        </td> 
+                    </tr> 
+                    </tbody> 
+                    </table></td> 
+                    </tr> 
+                    </tbody> 
+                    </table></td> 
+                    </tr> 
+                  </tbody> 
+                </table> 
+              </div> 
+               </td> 
+            </tr> 
+            </tbody> 
+            </table>
+            <table role="presentation" bgcolor="#ffffff" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
+            <tbody>
+            <tr> 
+            <td  style="padding-top:40px;padding-bottom:0px;padding-left:50px;padding-right:50px;font-family:sans-serif;font-size:12px;line-height:20px;color:#232323;text-align:left;font-weight:normal">
+         
+                  <p>You are receiving this email because you are subscribed to {{ config("app.CompanyName") }}.</p>
 
-</head>
-<title>Untitled Document</title>
+                  <p>We value your privacy and you may click on the <a href="#" target="_blank">Privacy Policy</a>.</p>
 
-<body>
+                    <br />
+                    <br />
 
-<style>
-    body {
-        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        background: #f0f0f0;
-    }
+                    <p>If you wish to unsubscriber from our list, you may click <a href="#">Unsubscribe</a></p>                                                  
+                 
+            </td> 
+            </tr> 
+            </tbody> 
+            </table>
+         
+              <table role="presentation" bgcolor="#ffffff" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
+            <tbody>
+            <tr> 
+            <td  style="padding-top:10px;padding-bottom:40px;padding-left:50px;padding-right:50px;font-family:sans-serif;font-size:12px;line-height:20px;color:#232323;text-align:left;font-weight:normal">
+             
+                 <br><br>
+                 Thank you,
+                 <br><br>
+                 {{ config("app.CompanyName") }}
 
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6,
-    p {
-        margin: 10px 0;
-        padding: 0;
-        font-weight: normal;
-    }
+            </td> 
+            </tr> 
+            </tbody> 
+            </table>
+            <table role="presentation" bgcolor="#f5f6f6" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
+            <tbody> 
+            <tr> 
+            <td  align="center" style="padding-top:30px;padding-bottom:30px"> 
+            <div > 
+            <a href="#"></a> 
+            <table role="presentation" width="600" cellpadding="0" cellspacing="0" border="0"  align="center" style="width:600px"> 
+            <tbody> 
+            <tr> 
+            <td> 
+             
+            <table role="presentation" width="160" align="left" cellpadding="0" cellspacing="0" border="0"  style="width:600px"> 
+            <tbody> 
+            <tr> 
+            <td valign="top" style="padding-left:50px;padding-right:12px;font-family:sans-serif;font-size:12px;line-height:14px;color:#67788a;text-align:left;font-weight:normal"> 
+            
+            <div>
+              This is a system generated email. Please do not reply on this email.
+            </div> 
+             <br>
+            <div>
+             Add us to your address book 
+            </div>
 
-    p {
-        font-size: 13px;
-    }
-</style>
+            <br>
 
-<!-- BODY-->
-<div style="max-width: 700px; width: 100%; background: #fff;margin: 30px auto;">
+             <div>
+             <a target="_blanlk" href="#">Privacy Policy </a> | 
+             <a target="_blanlk" href="#">Terms & Condition </a> | 
+              <a target="_blanlk" href="#">Unsubscribe </a> 
+            </div> 
 
-    <div style="padding:30px 60px;">
-        <div style="text-align: center;padding: 20px 0;">
-             <img src="https://beta.ebooklat.phr.com.ph/storage/logos/ebooklat-logo.png" style="width: 100px;height: 100px;">
-        </div>
-
-        <p>You are receiving this email because you are subscribed to {{ config("app.CompanyName") }}.</p>
-
-        <p>We value your privacy and you may click on the <a href="#" target="_blank">Privacy Policy</a>.</p>
-
-        <br />
-        <br />
-
-        <p>If you wish to unsubscriber from our list, you may click <a href="#">Unsubscribe</a></p>
-
-    </div>
-
-    <div style="padding: 30px;background: #fff;margin-top: 20px;border-top: solid 1px #eee;text-align: center;color: #aaa;">
-        <p style="font-size: 12px;">
-            <br />
-            <br />
-            <strong>{{ config("app.CompanyName") }}</strong> <br /> {{config('app.CompanyAddress')}} <br /> {{config('app.CompanyTelephoneNo')}} | {{config('app.CompanyMobileNo')}}
-
-            <br /><br /> {{ url('/') }}
-        </p>
-    </div>
+            </td> 
+            </tr> 
+            </tbody> 
+            </table> 
+            </td> 
+            </tr> 
+            </tbody> 
+            </table> 
+            </div> </td> 
+            </tr> 
+            </tbody> 
+            </table>
+             </td> 
+            </tr> 
+            </tbody>
+          </table> 
+        </td> 
+       </tr> 
+    </tbody>
+  </table> 
+  </div>  
+ </div>
 </div>
-
-</body>
-
-</html>
+</div>
