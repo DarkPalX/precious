b0bf168957ef0d8c3252db1a49735a1362f33852
ariz-kate <kate_paran@yahoo.com>
2024-10-18 15:02:23 +0800

    Update BannerAds.php
    fix

--- app/Models/APIModels/BannerAds.php
+++ app/Models/APIModels/BannerAds.php
@@ -58,8 +58,8 @@ class BannerAds extends Model
   public function getRandomPopUpBanner($data){
 
     $BannerID=$data['BannerID'];
-    $RecordBanner=$data['RecordBanner'];
-    
+    $BannerRecord=$data['BannerRecord'];
+
      $query = DB::table('mobile_banners as mob_ban')
      ->join('mobile_albums as  mob_alb', 'mob_alb.id', '=', 'mob_ban.album_id') 
     
@@ -83,13 +83,46 @@ class BannerAds extends Model
        $query->where("mob_alb.banner_type","=",'image'); 
        $query->where("mob_ban.deleted_at","=",null); 
        
-       if($BannerID>0 && $RecordBanner>1){
-          $query->where("mob_ban.id","!=",$BannerID);  
+       if($BannerID>0 && $BannerRecord>0){
+          $query->whereNotIn("mob_alb.status","=",1);  
        }
 
        $query->whereRaw('RAND()');   
       
-    $list = $query->first();
+    $info = $query->first();
+                             
+     return $info;  
+
+    
+  }
+
+    public function getPopUpBannerList(){
+
+   
+     $query = DB::table('mobile_banners as mob_ban')
+     ->join('mobile_albums as  mob_alb', 'mob_alb.id', '=', 'mob_ban.album_id') 
+    
+       ->selectraw("
+          mob_ban.id as banner_ID,
+          COALESCE(mob_ban.album_id,'') as album_id,
+
+          COALESCE(mob_ban.title,'') as title,
+          COALESCE(mob_ban.description,'') as description,
+          COALESCE(mob_ban.alt,'') as alt,
+          COALESCE(mob_ban.image_path,'') as image_path,
+          
+          COALESCE(mob_ban.button_text,'') as button_text,
+          COALESCE(mob_ban.url,'') as url_link,          
+          COALESCE(mob_ban.order,0) as order_sequence
+        ");    
+       
+       $query->where("mob_alb.status","=",1);  
+       $query->where("mob_ban.album_id","=",1); 
+       $query->where("mob_alb.type","=",'main_banner');    
+       $query->where("mob_alb.banner_type","=",'image'); 
+       $query->where("mob_ban.deleted_at","=",null); 
+                  
+      $list = $query->get();
                              
      return $list;  
 
