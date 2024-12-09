404579393f8a01fc2f87562da401015574ce1f74
ariz-kate <kate_paran@yahoo.com>
2024-12-09 10:00:37 +0800

    Update Company.php
    fix

--- app/Models/APIModels/Company.php
+++ app/Models/APIModels/Company.php
@@ -68,7 +68,7 @@ class Company extends Model
         ");    
        
        $query->where("pg.status","=",'PUBLISHED');  
-       $query->where("pg.label","=",'Privacy Policy');           
+       $query->where("pg.id","=",18);           
        $query->where("pg.deleted_at","=",null);  
      
     $list = $query->first();                           
