96a68988e7f08f5aaff475cfc64183651f245e52
ariz-kate <kate_paran@yahoo.com>
2025-02-13 15:52:49 +0800

    Update app.php
    set to live account precious PayPal account set Environment to live also not sandbox

--- config/app.php
+++ config/app.php
@@ -291,23 +291,25 @@ return [
     'DebugMode' => '0',
     'EmailDebugMode' => '0',
 
-    //PayPal Setting SandBox Environment
-    'PayPalSandBoxEnvironmentMode'=>true,
-    'PayPalClientID'=>'AeBIXT1-kv9wfiws93iQQLuKMRYKv3ENXhnfixHpeV1xhiCbUvmoGpA4c7JmZqs1E7_WR3lHS_AfxRzH',
-    'PayPalSecretKey' =>'ENUU_-qN_S9kU3-K8jv8bI16NDQNAWtBFTrevAcHa9wyb0GBDfFuVayafXuzUoeLXES-wnePXnm-gLi3',
-    'PaypalReturnURL'=>'https://preciouspagesbookstore.com.ph/return',
-    'PaypalCancelURL'=>'https://preciouspagesbookstore.com.ph/cancel',
-    'PayPalCurrency'=>'PHP',
-    'PayPalCountryCode'=>'PH',
-
-    //PayPal Setting Live Environment
-    // 'PayPalSandBoxEnvironmentMode'=>false,
-    // 'PayPalClientID'=>'AQSUwHiZjPlyZiOFCtrHYc40G2ixNG3dcjxoMLA6UQIqPMxM-KhxK1lMiWGcVp79E_NDhzevaN6rf5xz',
-    // 'PayPalSecretKey' =>'EMY-ndnPN5W_68Hq60ZaWEP1sW_e1qMKyGmycu8NDp_XnWms1YW5OtfwLITV7ZItlQXY5aywwkE0-i1m',
+    //PayPal Setting SandBox Environment TEST NI Developer
+    // 'PayPalSandBoxEnvironmentMode'=>true,
+    // 'PayPalClientID'=>'AeBIXT1-kv9wfiws93iQQLuKMRYKv3ENXhnfixHpeV1xhiCbUvmoGpA4c7JmZqs1E7_WR3lHS_AfxRzH',
+    // 'PayPalSecretKey' =>'ENUU_-qN_S9kU3-K8jv8bI16NDQNAWtBFTrevAcHa9wyb0GBDfFuVayafXuzUoeLXES-wnePXnm-gLi3',
     // 'PaypalReturnURL'=>'https://preciouspagesbookstore.com.ph/return',
     // 'PaypalCancelURL'=>'https://preciouspagesbookstore.com.ph/cancel',
     // 'PayPalCurrency'=>'PHP',
     // 'PayPalCountryCode'=>'PH',
+
+    //===========================================================================
+
+    //PayPal Setting Live Environment NI PRECIOUS
+    'PayPalSandBoxEnvironmentMode'=>false,
+    'PayPalClientID'=>'AQA2j0P4zaDuAAp38hP4PxyuvEtiszJBbk65NArwODHigueyFuyypla9TxAWpZvSeZ7QoGQirW9Ezvn6',
+    'PayPalSecretKey' =>'EHAkFcrJ_QeYU9fa5lfwL3_WvmFwFJ31GRFOTKD2XJwud5wcSmYtICZp_ns2ud-gzX1VTKeHMhMSvW2F',
+    'PaypalReturnURL'=>'https://preciouspagesbookstore.com.ph/return',
+    'PaypalCancelURL'=>'https://preciouspagesbookstore.com.ph/cancel',
+    'PayPalCurrency'=>'PHP',
+    'PayPalCountryCode'=>'PH',
 
     //SHOW/HIDE SETTING
     'ShowGoogleLogin'=>'Y',
