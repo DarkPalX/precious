f5a72b9499df75a49c56bbbc5a78899dad866eff
DarkPalX <raevinjhonpalx@gmail.com>
2025-03-21 14:35:42 +0800

    reports update

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -204,22 +204,26 @@ class FrontController extends Controller
 
         \Mail::to($client['email'])->send(new InquiryMail(Setting::info(), $client));
 
-        $email_recipients = EmailRecipient::all();
-
-        foreach ($email_recipients as $email_recipient) {
-            if (filter_var($email_recipient->email, FILTER_VALIDATE_EMAIL)) {
-                // ✅ Send individually with queue
-                Mail::to($email_recipient->email)
-                    ->queue(new InquiryAdminMail(Setting::info(), $client, $email_recipient));
-
-                // ✅ Add a small delay to prevent SMTP throttling
-                usleep(500000);  // 0.5-second delay
-            } else {
-                Log::error('Invalid email: ' . $email_recipient->email);
-            }
-        }
-
         // $email_recipients = EmailRecipient::all();
+
+        // foreach ($email_recipients as $email_recipient) {
+        //     if (filter_var($email_recipient->email, FILTER_VALIDATE_EMAIL)) {
+        //         // ✅ Send individually with queue
+        //         Mail::to($email_recipient->email)
+        //             ->queue(new InquiryAdminMail(Setting::info(), $client, $email_recipient));
+
+        //         // ✅ Add a small delay to prevent SMTP throttling
+        //         usleep(500000);  // 0.5-second delay
+        //     } else {
+        //         Log::error('Invalid email: ' . $email_recipient->email);
+        //     }
+        // }
+
+        $email_recipients = EmailRecipient::all();
+        foreach ($email_recipients as $email_recipient) {
+            \Mail::to($email_recipient->email)->send(new InquiryAdminMail(Setting::info(), $client, $email_recipient));
+        }
+        
         // foreach ($email_recipients as $email_recipient) {
 
         //     if (filter_var($email_recipient->email, FILTER_VALIDATE_EMAIL)) {
@@ -234,6 +238,25 @@ class FrontController extends Controller
 
         return redirect()->back();
     }
+
+    
+
+    
+    // public function contact_us(Request $request)
+    // {
+    //     $email_recipients  = EmailRecipient::all();
+    //     $client = $request->all();
+
+    //     \Mail::to($client['email'])->send(new InquiryMail(Setting::info(), $client));
+
+    //     foreach ($email_recipients as $email_recipient) {
+    //         \Mail::to($email_recipient->email)->send(new InquiryAdminMail(Setting::info(), $client, $email_recipient));
+    //     }
+
+    //     session()->flash('success', 'Email sent!');
+
+    //     return redirect()->back();
+    // }
 
     
     // public function contact_us(Request $request)
