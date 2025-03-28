f7a09f852ef8371d562e099a9fb0419f479c7266
ariz-kate <kate_paran@yahoo.com>
2025-03-28 13:47:20 +0800

    Update BannerAds.php
    added function that get the list of banner ads

--- app/Models/APIModels/BannerAds.php
+++ app/Models/APIModels/BannerAds.php
@@ -20,7 +20,8 @@ use App\Models\APIModels\Misc;
 
 class BannerAds extends Model
 {
-  
+   
+   //SLIDER BANNERS ADS
   public function getHomeSliderBannerList($data){
 
      $query = DB::table('mobile_banners as mob_ban')
@@ -55,7 +56,7 @@ class BannerAds extends Model
     
   }
  
-//IMAGE ONLY  // REMOVE THIS ONCE ALL APPS IS APPROVED
+//POP UP BANNER ADS IMAGE ONLY  // REMOVE THIS ONCE ALL APPS IS APPROVED
   public function getPopUpBannerList($data){
    
    $query = DB::table('mobile_banners as mob_ban')
@@ -89,7 +90,7 @@ class BannerAds extends Model
     
   }
 
-//IMAGE & VIDEO LATEST FUNCTION
+// POP UP BANNER ADS IMAGE & VIDEO LATEST FUNCTION
   public function getPopUpBannerListAll($data){
    
    $query = DB::table('mobile_banners as mob_ban')
@@ -118,6 +119,33 @@ class BannerAds extends Model
        $query->where("mob_ban.deleted_at","=",null); 
              
       $query->orderBy("mob_ban.order","ASC");          
+     $list = $query->get();                           
+     return $list;     
+    
+  }
+
+  //PAGE BODY SECTION ADS
+  public function getBannerAds($data){
+
+
+   $Page=$data['Page'];
+   
+   $query = DB::table('banner_ads as ban_ads')
+     ->join('banner_ad_pages as  ban_ads_pg', 'ban_ads_pg.banner_ad_id', '=', 'ban_ads.id') 
+    
+       ->selectraw("
+           ban_ads.id as banner_Ads_ID,
+           COALESCE(mob_ban.mobile_file_url,'') as mobile_file_url,
+           COALESCE(mob_ban.url,'') as url,
+
+          COALESCE(mob_ban.click_counts,0) as click_counts
+                        
+        ");    
+              
+       $query->where("ban_ads_pg.page_id",'=',$Page);             
+       $query->where("ban_ads.status","=",1);           
+       $query->where("ban_ads_pg.deleted_at","=",null); 
+                   
      $list = $query->get();                           
      return $list;     
     
