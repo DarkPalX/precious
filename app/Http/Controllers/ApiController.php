f20d110987544844aa1ac1966491d9a7ef5ff6d1
ariz-kate <kate_paran@yahoo.com>
2025-04-02 10:28:36 +0800

    Update ApiController.php
    test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -50,10 +50,10 @@ class ApiController extends Controller {
     $request['APP_VERSION']='1.0.0';
 
     if($request['APP_TYPE']){
-        if($request['APP_TYPE'] == config('app.PLATFORM_ANDROID')){
+        if($request['APP_TYPE'] == 'Android'){
 
           if($request['APP_VERSION']){
-            if($request['APP_VERSION'] == config('app.AndroidLatestVersion') || $request['APP_VERSION'] == config('app.AndroidBetaVersion')){
+            if($request['APP_VERSION'] == '1.0.0' || $request['APP_VERSION'] == '1.0.0'){
                  $IsUpdated = true;
             }
           }
