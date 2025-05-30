e5f4de7dbad2c2bf5ed7cae387551e4fd60e0fe7
ariz-kate <kate_paran@yahoo.com>
2025-05-30 15:23:42 +0800

    Update ApiController.php
    fix

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1101,7 +1101,7 @@ class ApiController extends Controller {
 public function getAllCustomerLibraryData(Request $request){
     
     $Misc = new Misc();
-    $Book = new Book();
+    $Books= new Book();
     $Library = new Library();
 
     $response = "Failed";
