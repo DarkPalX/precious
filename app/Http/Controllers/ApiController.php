a4b09b7730be7f51938ce0cfb8a07317d0e1ba57
ariz-kate <kate_paran@yahoo.com>
2025-08-22 11:07:25 +0800

    Update ApiController.php
    remove test value

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1434,16 +1434,7 @@ public function saveReadBookCount(Request $request){
     $responseMessage = "";
 
     $data['ProductID']=$request->post('ProductID'); 
-    $data['ProductID']=135; 
-
-    //  if(empty($data['ProductID'])){
-    //   $ResponseMessage = 'Product ID is required.';
-    //    return response()->json([
-    //        'response' => 'Failed',
-    //        'message' => $ResponseMessage,
-    //    ]);
-    // }
-
+ 
     $result=$Book->saveReadBookCount($data);  
     return response()->json($result); 
     
