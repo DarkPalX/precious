120210dfc9db727f28818f56eb41a2fe9870bb4d
ariz-kate <kate_paran@yahoo.com>
2025-08-26 15:15:20 +0800

    added read count

--- app/Models/APIModels/Book.php
+++ app/Models/APIModels/Book.php
@@ -593,6 +593,7 @@ class Book extends Model
           COALESCE(prds.ebook_discount_price,0) as discount_price,      
           
           COALESCE(prds.reorder_point,0) as reorder_point,  
+          COALESCE(prds.read_count,0) as read_count,  
 
           CONCAT(COALESCE(prds.name,''),' ', COALESCE(prds.author,''),'', COALESCE(prds.book_type,'') ,'', COALESCE(prds.subtitle,'')) as search_fields,
 
