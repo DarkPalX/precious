1b24befc31bc8be13ffe99c8f102afaaad9fb465
DarkPalX <raevinjhonpalx@gmail.com>
2025-12-11 16:17:28 +0800

    remove hardcoded link on menu physical and ebook links

--- resources/views/theme/layouts/components/menu.blade.php
+++ resources/views/theme/layouts/components/menu.blade.php
@@ -9,12 +9,12 @@
         @include('theme.layouts.components.menu-item', ['item' => $item])
     @endforeach
 
-    <li class="menu-item">
+    {{-- <li class="menu-item">
         <a class="menu-link" href="{{ route('product.front.list') }}"><div>Physical Books</div></a>
     </li>
     <li class="menu-item">
         <a class="menu-link" href="{{ route('product.front.ebook-list') }}"><div>E-Books</div></a>
-    </li>
+    </li> --}}
     {{-- <li class="menu-item" @if(!auth()->user()) hidden @endif>
         <a class="menu-link" href="{{ route('customer.subscription') }}"><div>Subscriptions</div></a>
     </li> --}}
