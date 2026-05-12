c7e783cccf5773aec41f5cfc17b2841dbba3c2ca
DarkPalX <raevinjhonpalx@gmail.com>
2026-05-12 16:36:04 +0800

    updated reports

--- app/Http/Controllers/Ecommerce/ReportsController.php
+++ app/Http/Controllers/Ecommerce/ReportsController.php
@@ -400,7 +400,8 @@ class ReportsController extends Controller
     public function sales_list_mobile(Request $request)
     {
         $sales = SalesDetail::join('ecommerce_sales_headers', 'ecommerce_sales_details.sales_header_id', 'ecommerce_sales_headers.id')
-            ->where('order_source', 'Android')
+            ->where('order_source', '<>', 'Android')
+            ->orWhereNull('order_source')
             ->whereNotNull('ecommerce_sales_headers.id');
 
 
@@ -437,6 +438,47 @@ class ReportsController extends Controller
         return view('admin.ecommerce.reports-mobile.sales-transaction',compact('sales', 'startDate', 'endDate', 'customer', 'product', 'category', 'status'));
 
     }
+
+    // public function sales_list_mobile(Request $request)
+    // {
+    //     $sales = SalesDetail::join('ecommerce_sales_headers', 'ecommerce_sales_details.sales_header_id', 'ecommerce_sales_headers.id')
+    //         ->where('order_source', 'Android')
+    //         ->whereNotNull('ecommerce_sales_headers.id');
+
+
+    //     $startDate = $request->get('start', false);
+    //     $endDate   = $request->get('end', false);
+    //     $customer  = $request->get('customer', false);
+    //     $product   = $request->get('product', false);
+    //     $category   = $request->get('category', false);
+    //     $status    = $request->get('del_status', false);
+
+
+    //     if(isset($customer) && $customer <> ''){
+    //         $sales->where('ecommerce_sales_headers.customer_name', $customer);
+    //     }
+
+    //     if(isset($product) && $product <> ''){
+    //         $sales->where('ecommerce_sales_details.product_name', $product);
+    //     }
+
+    //     if(isset($category) && $category <> ''){
+    //         $sales->where('ecommerce_sales_details.product_category', $category);
+    //     }
+
+    //     if(isset($status) && $status <> ''){
+    //         $sales->where('ecommerce_sales_headers.delivery_status', $status);
+    //     }
+      
+    //     if(isset($startDate) && strlen($startDate)>=1){
+    //         $sales->whereBetween('ecommerce_sales_headers.created_at',[$startDate." 00:00:00.000", $endDate." 23:59:59.999"]);  
+    //     }
+
+    //     $sales = $sales->orderBy('ecommerce_sales_headers.created_at', 'desc')->paginate($this->pageCount);
+
+    //     return view('admin.ecommerce.reports-mobile.sales-transaction',compact('sales', 'startDate', 'endDate', 'customer', 'product', 'category', 'status'));
+
+    // }
     
     public function top_buyers_mobile(Request $request)
     {       
