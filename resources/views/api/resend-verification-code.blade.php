ba78f7aeb198a49f77a980d002e75db754673307
ariz-kate <kate_paran@yahoo.com>
2026-07-23 11:40:35 +0800

    Update resend-verification-code.blade.php
    update style and new logo catha

--- resources/views/api/resend-verification-code.blade.php
+++ resources/views/api/resend-verification-code.blade.php
@@ -1,169 +1,26 @@
-     
-<div bgcolor="#fafafa">
-  <div style="background-color:#fafafa"> 
-  <table role="presentation" width="100%" bgcolor="#fafafa" cellpadding="0" cellspacing="0" border="0" align="center" style="margin:0px auto;width:100%!important;min-width:100%!important"> 
-    <tbody>
-      <tr> 
-        <td> 
-          <table role="presentation" cellpadding="0" cellspacing="0" border="0" width="600" align="center" style="margin:0px auto;width:600px"> 
-            <tbody>
-            <tr> 
-            <td align="center" valign="top">
-            <table role="presentation" bgcolor="#ffffff" width="100%" cellpadding="0" cellspacing="0" border="0" align="center"   style="margin:0px auto;width:100%!important;min-width:100%!important"> 
-              <tbody></tbody>  
-            </table>
-            <table role="presentation" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
-            <tbody> 
-            <tr> 
-              <td dir="rtl" style="text-align:center;font-size:0;background:#2e3c5f"> 
-              <div  bgcolor="#ffffff" style="max-width:200px;width:100%;display:inline-block;vertical-align:top" dir="ltr"> 
-              <table bgcolor="#2e3c5f" width="100%" bgcolor="#ffffff" valign="top" border="0" cellpadding="0" cellspacing="0" style="border-collapse:collapse"> 
-                  <tbody> 
-                    <tr> 
-                    <td style="line-height:1px"> 
-                      <table role="presentation" width="250" height="140" align="center" cellpadding="0" cellspacing="0" border="0"  style="width:250px;height:140px">
-                      <tbody> 
-                      <tr> 
-                        <td height="140" valign="bottom"> 
-                          <div style="display:inline-block">                              
-                             <img src="https://preciouspagesbookstore.com.ph/storage/logos/ebooklat-logo-horizontal.png" style="width: 106%;">                       
-                          </div> 
-                      </td> 
-                      </tr> 
-                      </tbody> 
-                      </table> 
-                     </td> 
-                    </tr> 
-                  </tbody> 
-                </table> 
-              </div> 
-               
-               
-              <div  bgcolor="#677b8c" style="max-width:300px;width:100%;display:inline-block;vertical-align:top" dir="ltr"> 
-                <table width="100%" bgcolor="#677b8c" align="left" valign="top" border="0" cellpadding="0" cellspacing="0" style="border-collapse:collapse"> 
-                  <tbody> 
-                    <tr> 
-                    <td valign="middle"> 
-                    <table role="presentation" width="450" height="140" align="center" cellpadding="0" cellspacing="0" border="0"  style="width:450px;height:140px"> 
-                    <tbody> 
-                    <tr> 
-                    <td  bgcolor="#2e3c5f" valign="middle" height="140" style="height:140px" > 
-                    <table role="presentation" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
-                    <tbody> 
-                    <tr> 
-                    <td  valign="top" style="padding-left:50px;padding-right:35px;font-family:sans-serif;font-size:24px;line-height:32px;color:#ffffff;text-align:left;font-weight:normal">
-            
-                          <br>
-                          <br>
-                            New Verification Code
-                        </td> 
-                    </tr> 
-                    </tbody> 
-                    </table></td> 
-                    </tr> 
-                    </tbody> 
-                    </table></td> 
-                    </tr> 
-                  </tbody> 
-                </table> 
-              </div> 
-               </td> 
-            </tr> 
-            </tbody> 
-            </table>
-            <table role="presentation" bgcolor="#ffffff" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
-            <tbody>
-            <tr> 
-            <td  style="padding-top:40px;padding-bottom:0px;padding-left:50px;padding-right:50px;font-family:sans-serif;font-size:12px;line-height:20px;color:#232323;text-align:left;font-weight:normal">
-               Dear Customer,
-              <br><br>
-                  
-                  We noticed that you haven't yet verified your account with us at {{ config("app.CompanyName") }}. To complete the registration process, 
-                  please enter this new 4 digit verification code below:
-                  
-                  <br><br>
-                  
-                  CODE: <b>{{ $VerificationCode }}</b> 
-                  
-                  <br><br>
-                  If you did not request this verification code, please disregard this email.    <br><br>              
-                  Should you have any questions or encounter any issues, feel free to reach out to our customer service team —we're here to help!
-
-            </td> 
-            </tr> 
-            </tbody> 
-            </table>
-         
-              <table role="presentation" bgcolor="#ffffff" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
-            <tbody>
-            <tr> 
-            <td  style="padding-top:10px;padding-bottom:40px;padding-left:50px;padding-right:50px;font-family:sans-serif;font-size:12px;line-height:20px;color:#232323;text-align:left;font-weight:normal">
-             
-                 <br><br>
-                 Thank you,
-                 <br><br>
-                 {{ config("app.CompanyName") }} <br>
-                  Admin & Team Staff 
-
-            </td> 
-            </tr> 
-            </tbody> 
-            </table>
-            <table role="presentation" bgcolor="#f5f6f6" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
-            <tbody> 
-            <tr> 
-            <td  align="center" style="padding-top:30px;padding-bottom:30px"> 
-            <div > 
-            <a href="#"></a> 
-            <table role="presentation" width="600" cellpadding="0" cellspacing="0" border="0"  align="center" style="width:600px"> 
-            <tbody> 
-            <tr> 
-            <td> 
-             
-            <table role="presentation" width="160" align="left" cellpadding="0" cellspacing="0" border="0"  style="width:600px"> 
-            <tbody> 
-            <tr> 
-            <td valign="top" style="padding-left:50px;padding-right:12px;font-family:sans-serif;font-size:12px;line-height:14px;color:#67788a;text-align:left;font-weight:normal"> 
-            
-            <div>
-              This is a system generated email. Please do not reply on this email.
-            </div> 
-            
-            <br>
-
-            <div>
-             Add us to your address book 
-            </div>
-
-            <br>
-
-           <div>
-             <a target="_blanlk" href="https://preciouspagesbookstore.com.ph/privacy-policy">Privacy Policy </a> | 
-             <a target="_blanlk" href="https://preciouspagesbookstore.com.ph/terms-of-use-agreement">Terms & Condition </a> | 
-              <a target="_blanlk" href="javascript:void(0);">Unsubscribe </a> 
-            </div> 
-
-            </td> 
-            </tr> 
-            </tbody> 
-            </table> 
-            </td> 
-            </tr> 
-            </tbody> 
-            </table> 
-            </div> </td> 
-            </tr> 
-            </tbody> 
-            </table>
-             </td> 
-            </tr> 
-            </tbody>
-          </table> 
-        </td> 
-       </tr> 
-    </tbody>
-  </table> 
-  </div>  
- </div>
-</div>
-</div>
+<table role="presentation" width="100%" cellpadding="0" cellspacing="0" bgcolor="#fafafa">
+<tr><td align="center">
+<table role="presentation" width="600" cellpadding="0" cellspacing="0" bgcolor="#ffffff" style="margin:30px auto;border-radius:8px;">
+<tr><td bgcolor="#7E57C2" style="padding:30px 40px;">
+<table width="100%"><tr>
+<td width="180"><img src="https://preciouspagesbookstore.com.ph/storage/logos/catha-email-logo.png" width="170"></td>
+<td style="border-left:2px solid #9b7ad1;padding-left:25px;color:#fff;">
+<div style="font-size:30px;font-weight:bold;">New Verification Code</div>
+<div style="font-size:14px;color:#efe7f8;padding-top:8px;">Verify your account to continue.</div>
+</td>
+</tr></table>
+</td></tr>
+<tr><td style="padding:40px 50px;font-size:14px;line-height:24px;color:#333;">
+<p>Dear Customer,</p>
+<p>We noticed that you haven't yet verified your account with us at {{ config("app.CompanyName") }}. Please use the verification code below:</p>
+<table align="center" cellpadding="0" cellspacing="0"><tr><td style="border:2px dashed #7E57C2;background:#F4F0FA;padding:18px 40px;font-size:34px;letter-spacing:8px;color:#6F42C1;font-weight:bold;">{{ $VerificationCode }}</td></tr></table>
+<p>If you did not request this verification code, please ignore this email.</p>
+<p>Thank you,<br>{{ config("app.CompanyName") }}<br>Admin & Team Staff</p>
+</td></tr>
+<tr><td bgcolor="#f5f6f6" style="padding:25px 40px;font-size:12px;color:#666;line-height:20px;">
+This is a system generated email. Please do not reply.<br><br>
+<a href="https://preciouspagesbookstore.com.ph/privacy-policy">Privacy Policy</a> |
+<a href="https://preciouspagesbookstore.com.ph/terms-of-use-agreement">Terms & Conditions</a> |
+<a href="#">Unsubscribe</a>
+</td></tr>
+</table>
