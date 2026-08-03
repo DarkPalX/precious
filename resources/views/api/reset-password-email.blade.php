4506c45887d0d089f4df5d05cb6cccfe51ea8f3f
ariz-kate <kate_paran@yahoo.com>
2026-08-03 10:26:08 +0800

    Update reset-password-email.blade.php
    revise password reset template

--- resources/views/api/reset-password-email.blade.php
+++ resources/views/api/reset-password-email.blade.php
@@ -1,162 +1,98 @@
+<table role="presentation" width="100%" cellpadding="0" cellspacing="0" bgcolor="#fafafa">
+<tr>
+<td align="center">
 
-<div bgcolor="#fafafa">
-  <div style="background-color:#fafafa"> 
-  <table role="presentation" width="100%" bgcolor="#fafafa" cellpadding="0" cellspacing="0" border="0" align="center" style="margin:0px auto;width:100%!important;min-width:100%!important"> 
-    <tbody>
-      <tr> 
-        <td> 
-          <table role="presentation" cellpadding="0" cellspacing="0" border="0" width="600" align="center" style="margin:0px auto;width:600px"> 
-            <tbody>
-            <tr> 
-            <td align="center" valign="top">
-            <table role="presentation" bgcolor="#ffffff" width="100%" cellpadding="0" cellspacing="0" border="0" align="center"   style="margin:0px auto;width:100%!important;min-width:100%!important"> 
-              <tbody></tbody>  
+<table role="presentation" width="600" cellpadding="0" cellspacing="0" bgcolor="#ffffff" style="margin:30px auto;border-radius:8px;">
+
+    <!-- Header -->
+    <tr>
+        <td bgcolor="#7E57C2" style="padding:30px 40px;">
+            <table width="100%" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td width="180">
+                        <img src="https://preciouspagesbookstore.com.ph/storage/logos/catha-email-logo.png" width="170" alt="Catha Logo">
+                    </td>
+
+                    <td style="border-left:2px solid #9b7ad1;padding-left:25px;color:#ffffff;">
+                        <div style="font-size:30px;font-weight:bold;">
+                            Password Reset
+                        </div>
+
+                        <div style="font-size:14px;color:#efe7f8;padding-top:8px;">
+                            Your temporary password is ready.
+                        </div>
+                    </td>
+                </tr>
             </table>
-            <table role="presentation" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
-            <tbody> 
-            <tr> 
-              <td dir="rtl" style="text-align:center;font-size:0;background:#2e3c5f"> 
-              <div  bgcolor="#ffffff" style="max-width:200px;width:100%;display:inline-block;vertical-align:top" dir="ltr"> 
-              <table bgcolor="#2e3c5f" width="100%" bgcolor="#ffffff" valign="top" border="0" cellpadding="0" cellspacing="0" style="border-collapse:collapse"> 
-                  <tbody> 
-                    <tr> 
-                    <td style="line-height:1px"> 
-                      <table role="presentation" width="250" height="140" align="center" cellpadding="0" cellspacing="0" border="0"  style="width:250px;height:140px">
-                      <tbody> 
-                      <tr> 
-                        <td height="140" valign="bottom" style="text-align:left;height:140px"> 
-                          <div style="display:inline-block">                              
-                            <img src="https://preciouspagesbookstore.com.ph/storage/logos/ebooklat-logo-horizontal.png" style="width: 106%;">                            
-                          </div> 
-                      </td> 
-                      </tr> 
-                      </tbody> 
-                      </table> 
-                     </td> 
-                    </tr> 
-                  </tbody> 
-                </table> 
-              </div> 
-               
-              <div  bgcolor="#677b8c" style="max-width:300px;width:100%;display:inline-block;vertical-align:top" dir="ltr"> 
-                <table width="100%" bgcolor="#677b8c" align="left" valign="top" border="0" cellpadding="0" cellspacing="0" style="border-collapse:collapse"> 
-                  <tbody> 
-                    <tr> 
-                    <td valign="middle"> 
-                    <table role="presentation" width="450" height="140" align="center" cellpadding="0" cellspacing="0" border="0"  style="width:450px;height:140px"> 
-                    <tbody> 
-                    <tr> 
-                    <td bgcolor="#2e3c5f" valign="middle" height="140" style="height:140px" > 
-                    <table role="presentation" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
-                    <tbody> 
-                    <tr> 
-                    <td  valign="top" style="padding-left:50px;padding-right:35px;font-family:sans-serif;font-size:24px;line-height:32px;color:#ffffff;text-align:left;font-weight:normal">
-            
-                          <br>
-                          <br>
-                              Reset Password
-                        </td> 
-                    </tr> 
-                    </tbody> 
-                    </table></td> 
-                    </tr> 
-                    </tbody> 
-                    </table></td> 
-                    </tr> 
-                  </tbody> 
-                </table> 
-              </div> 
-               </td> 
-            </tr> 
-            </tbody> 
+        </td>
+    </tr>
+
+    <!-- Body -->
+    <tr>
+        <td style="padding:40px 50px;font-size:14px;line-height:24px;color:#333333;">
+
+            <p>Dear Customer,</p>
+
+            <p>
+                We received a request to reset your account password.
+                For your security, a temporary password has been generated.
+                Please use it to log in to your account, then change your password immediately.
+            </p>
+
+            <table align="center" cellpadding="0" cellspacing="0" style="margin:30px auto;">
+                <tr>
+                    <td style="border:2px dashed #7E57C2;background:#F4F0FA;padding:18px 40px;font-size:24px;color:#6F42C1;font-weight:bold;text-align:center;">
+                        {{ $Password }}
+                    </td>
+                </tr>
             </table>
-            <table role="presentation" bgcolor="#ffffff" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
-            <tbody>
-            <tr> 
-            <td  style="padding-top:40px;padding-bottom:0px;padding-left:50px;padding-right:50px;font-family:sans-serif;font-size:12px;line-height:20px;color:#232323;text-align:left;font-weight:normal">
 
-               Dear <b> Customer </b>,
-              <br><br>
+            <p>
+                For your protection, please delete this email after successfully logging in and changing your password.
+            </p>
 
-                  Due to the encryption method used by our system, we issued a new and temporary password for you. 
-                  Please change your password after logging in to your account and delete this email immediately.<br><br>
-
-                   New Password :
-                   <br>
-                    <span style="color:#0b389d;font-weight:bold;"><b>{{ $Password }}</b></span>
-
-            </td> 
-            </tr> 
-            </tbody> 
-            </table>
-         
-              <table role="presentation" bgcolor="#ffffff" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
-            <tbody>
-            <tr> 
-            <td  style="padding-top:10px;padding-bottom:40px;padding-left:50px;padding-right:50px;font-family:sans-serif;font-size:12px;line-height:20px;color:#232323;text-align:left;font-weight:normal">
-             
-                Thank you,
-                 <br><br>
-                 {{ config("app.CompanyName") }} <br>
-                  Admin & Team Staff 
-
-            </td> 
-            </tr> 
-            </tbody> 
-            </table>
-            <table role="presentation" bgcolor="#f5f6f6" width="100%" align="center" cellpadding="0" cellspacing="0" border="0"  style="margin:0px auto;width:100%!important;min-width:100%!important"> 
-            <tbody> 
-            <tr> 
-            <td  align="center" style="padding-top:30px;padding-bottom:30px"> 
-            <div > 
-            <a href="#"></a> 
-            <table role="presentation" width="600" cellpadding="0" cellspacing="0" border="0"  align="center" style="width:600px"> 
-            <tbody> 
-            <tr> 
-            <td> 
-             
-            <table role="presentation" width="160" align="left" cellpadding="0" cellspacing="0" border="0"  style="width:600px"> 
-            <tbody> 
-            <tr> 
-            <td valign="top" style="padding-left:50px;padding-right:12px;font-family:sans-serif;font-size:12px;line-height:14px;color:#67788a;text-align:left;font-weight:normal"> 
-            
-            <div>
-              This is a system generated email. Please do not reply on this email.
-            </div> 
-             <br>
-            <div>
-             Add us to your address book 
-            </div>
+            <p>
+                If you did not request this password reset, please ignore this email or contact our support team immediately.
+            </p>
 
             <br>
 
-             <div>
-             <a target="_blanlk" href="https://preciouspagesbookstore.com.ph/privacy-policy">Privacy Policy </a> | 
-             <a target="_blanlk" href="https://preciouspagesbookstore.com.ph/terms-of-use-agreement">Terms & Condition </a> | 
-              <a target="_blanlk" href="javascript:void(0);">Unsubscribe </a> 
-            </div> 
+            <p>
+                Thank you,<br>
+                <strong>{{ config("app.CompanyName") }}</strong><br>
+                Admin & Team Staff
+            </p>
 
-            </td> 
-            </tr> 
-            </tbody> 
-            </table> 
-            </td> 
-            </tr> 
-            </tbody> 
-            </table> 
-            </div> </td> 
-            </tr> 
-            </tbody> 
-            </table>
-             </td> 
-            </tr> 
-            </tbody>
-          </table> 
-        </td> 
-       </tr> 
-    </tbody>
-  </table> 
-  </div>  
- </div>
-</div>
-</div>
+        </td>
+    </tr>
+
+    <!-- Footer -->
+    <tr>
+        <td bgcolor="#f5f6f6" style="padding:25px 40px;font-size:12px;color:#666666;line-height:20px;">
+
+            This is a system generated email. Please do not reply.
+
+            <br><br>
+
+            <a href="https://preciouspagesbookstore.com.ph/privacy-policy">
+                Privacy Policy
+            </a>
+            &nbsp;|&nbsp;
+
+            <a href="https://preciouspagesbookstore.com.ph/terms-of-use-agreement">
+                Terms &amp; Conditions
+            </a>
+            &nbsp;|&nbsp;
+
+            <a href="#">
+                Unsubscribe
+            </a>
+
+        </td>
+    </tr>
+
+</table>
+
+</td>
+</tr>
+</table>
