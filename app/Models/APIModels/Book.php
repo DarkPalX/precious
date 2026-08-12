88f459bfc937db82df035c1f36e4ee059898da17
ariz-kate <kate_paran@yahoo.com>
2026-08-13 01:37:10 +0800

    Update Book.php
    update queryy

--- app/Models/APIModels/Book.php
+++ app/Models/APIModels/Book.php
@@ -25,13 +25,15 @@ class Book extends Model
   
   public function getBookList($data){
 
-    $UserID=$data['UserID'];
+   // $UserID=$data['UserID'];
 
     $Status=$data['Status'];
     $SearchText=$data['SearchText'];
     
     $Limit=$data['Limit'];
     $PageNo=$data['PageNo'];
+   
+    $UserID = isset($data['UserID']) ? (int)$data['UserID'] : 0;
 
     $query = DB::table('products as prds')
     ->join('product_categories as prod_cat', 'prod_cat.id', '=', 'prds.category_id') 
@@ -144,7 +146,7 @@ class Book extends Model
                     INNER JOIN products AS prods
                         ON prods.id = bkmrk.product_id
                     WHERE bkmrk.product_id = prds.id
-                      AND bkmrk.customer_id=".$UserID." 
+                      AND bkmrk.customer_id= ?
                     LIMIT 1
                 ),
                 ''
@@ -156,7 +158,7 @@ class Book extends Model
                     SELECT cust_lib.product_id
                     FROM customer_libraries AS cust_lib
                     WHERE cust_lib.product_id = prds.id
-                      AND cust_lib.user_id=".$UserID."
+                      AND cust_lib.user_id= ? 
                     LIMIT 1
                 ),
                 0
@@ -164,7 +166,7 @@ class Book extends Model
 
             COALESCE(prds.status, '') AS status          
           
-        ");    
+      ", [$UserID, $UserID]);
 
       $query->where("prds.file_url","!=",null); 
       $query->where("prds.status","=",'PUBLISHED');    
@@ -225,13 +227,15 @@ class Book extends Model
 
   public function getRandomBookList($data){
 
-    $UserID=$data['UserID'];
+    //$UserID=$data['UserID'];
 
     $Status=$data['Status'];
     $SearchText=$data['SearchText'];
     
     $Limit=$data['Limit'];
     $PageNo=$data['PageNo'];
+
+    $UserID = isset($data['UserID']) ? (int)$data['UserID'] : 0;
 
     $query = DB::table('products as prds')
     ->join('product_categories as prod_cat', 'prod_cat.id', '=', 'prds.category_id') 
@@ -344,7 +348,7 @@ class Book extends Model
                     INNER JOIN products AS prods
                         ON prods.id = bkmrk.product_id
                     WHERE bkmrk.product_id = prds.id
-                      AND bkmrk.customer_id=".$UserID." 
+                      AND bkmrk.customer_id= ?
                     LIMIT 1
                 ),
                 ''
@@ -356,7 +360,7 @@ class Book extends Model
                     SELECT cust_lib.product_id
                     FROM customer_libraries AS cust_lib
                     WHERE cust_lib.product_id = prds.id
-                      AND cust_lib.user_id=".$UserID."
+                      AND cust_lib.user_id= ?
                     LIMIT 1
                 ),
                 0
@@ -364,7 +368,7 @@ class Book extends Model
 
             COALESCE(prds.status, '') AS status          
           
-        ");    
+     ", [$UserID, $UserID]);   
 
       $query->where("prds.file_url","!=",null); 
       $query->where("prds.status","=",'PUBLISHED');    
@@ -379,12 +383,15 @@ class Book extends Model
 
   public function getContinueToReadBookList($data){
 
-    $UserID=$data['UserID'];
+    //$UserID=$data['UserID'];
+
     $Status=$data['Status'];
     $SearchText=$data['SearchText'];
     
     $Limit=$data['Limit'];
     $PageNo=$data['PageNo'];
+
+    $UserID = isset($data['UserID']) ? (int)$data['UserID'] : 0;
 
       $query = DB::table('continue_to_read_book as cont')  
       ->join('products as prds', 'prds.id', '=', 'cont.product_id')   
@@ -497,7 +504,7 @@ class Book extends Model
                     INNER JOIN products AS prods
                         ON prods.id = bkmrk.product_id
                     WHERE bkmrk.product_id = prds.id
-                      AND bkmrk.customer_id=".$UserID." 
+                      AND bkmrk.customer_id= ?
                     LIMIT 1
                 ),
                 ''
@@ -509,7 +516,7 @@ class Book extends Model
                     SELECT cust_lib.product_id
                     FROM customer_libraries AS cust_lib
                     WHERE cust_lib.product_id = prds.id
-                      AND cust_lib.user_id=".$UserID."
+                      AND cust_lib.user_id= ?
                     LIMIT 1
                 ),
                 0
@@ -517,7 +524,7 @@ class Book extends Model
 
             COALESCE(prds.status, '') AS status        
           
-        ");    
+      ", [$UserID, $UserID]);
   
       $query->where("prds.deleted_at","=",null);
       $query->whereRaw("cont.customer_id =?",[$UserID]); 
@@ -534,7 +541,7 @@ class Book extends Model
     $Status=$data['Status'];
     $SearchText=$data['SearchText'];
 
-    $UserID=$data['UserID'];
+    //$UserID=$data['UserID'];
 
     $Filter_Sort=$data['Filter_Sort'];
     $Filter_Genre=$data['Filter_Genre'];
@@ -542,6 +549,8 @@ class Book extends Model
     
     $Limit=$data['Limit'];
     $PageNo=$data['PageNo'];
+
+    $UserID = isset($data['UserID']) ? (int)$data['UserID'] : 0;
 
     $query = DB::table('products as prds')
     ->leftjoin('product_categories as prod_cat', 'prod_cat.id', '=', 'prds.category_id') 
@@ -655,7 +664,7 @@ class Book extends Model
                     INNER JOIN products AS prods
                         ON prods.id = bkmrk.product_id
                     WHERE bkmrk.product_id = prds.id
-                      AND bkmrk.customer_id=".$UserID." 
+                      AND bkmrk.customer_id= ?
                     LIMIT 1
                 ),
                 ''
@@ -667,7 +676,7 @@ class Book extends Model
                     SELECT cust_lib.product_id
                     FROM customer_libraries AS cust_lib
                     WHERE cust_lib.product_id = prds.id
-                      AND cust_lib.user_id=".$UserID."
+                      AND cust_lib.user_id= ?
                     LIMIT 1
                 ),
                 0
@@ -675,7 +684,7 @@ class Book extends Model
 
             COALESCE(prds.status, '') AS status            
           
-        ");    
+        ", [$UserID, $UserID]);
 
       $query->where("prds.file_url","!=",null); 
       $query->where("prds.status","=",'PUBLISHED');    
@@ -1205,125 +1214,126 @@ class Book extends Model
  // CATALOGUE DETAILS
  public function getDetailsCatalogueList($data){
 
-      $HeaderID=$data['HeaderID'];
-      $UserID=$data['UserID'];
+    $HeaderID = $data['HeaderID'];
+       
+    $UserID = isset($data['UserID']) ? (int)$data['UserID'] : 0;
 
-      $query = DB::table('product_catalog_details as prod_det_cat')  
-      ->join('product_catalog_headers as prod_hdrs_cat', 'prod_hdrs_cat.id', '=', 'prod_det_cat.product_catalog_header_id')   
-      ->join('products as prds', 'prds.id', '=', 'prod_det_cat.product_id')   
+    $query = DB::table('product_catalog_details as prod_det_cat')  
+    ->join('product_catalog_headers as prod_hdrs_cat', 'prod_hdrs_cat.id', '=', 'prod_det_cat.product_catalog_header_id')   
+    ->join('products as prds', 'prds.id', '=', 'prod_det_cat.product_id')   
 
-       ->selectraw("    
-          prod_hdrs_cat.name  as header_name,
+     ->selectraw("    
+        prod_hdrs_cat.name  as header_name,
 
-          prds.id as book_ID,
+        prds.id as book_ID,
 
-          COALESCE(prds.name,'') as name,
-          COALESCE(prds.author,'') as author,
-          COALESCE(prds.subtitle,'') as subtitle,
-          COALESCE(prds.description,'') as  short_description,
-          
-          COALESCE(prds.slug,'') as slug,
-          COALESCE(prds.file_url,'') as file_url,          
+        COALESCE(prds.name,'') as name,
+        COALESCE(prds.author,'') as author,
+        COALESCE(prds.subtitle,'') as subtitle,
+        COALESCE(prds.description,'') as  short_description,
+        
+        COALESCE(prds.slug,'') as slug,
+        COALESCE(prds.file_url,'') as file_url,          
 
-          COALESCE(prds.category_id,0) as category_id,
-          COALESCE(prds.book_type,'') as book_type,          
-      
-          COALESCE(prds.sku,'') as sku,          
-          COALESCE(prds.size,'') as size,
-          COALESCE(prds.weight,'') as weight,
-          COALESCE(prds.texture,'') as texture,
-          COALESCE(prds.uom,'') as uom,
+        COALESCE(prds.category_id,0) as category_id,
+        COALESCE(prds.book_type,'') as book_type,          
+    
+        COALESCE(prds.sku,'') as sku,          
+        COALESCE(prds.size,'') as size,
+        COALESCE(prds.weight,'') as weight,
+        COALESCE(prds.texture,'') as texture,
+        COALESCE(prds.uom,'') as uom,
 
-          COALESCE(prds.is_featured,0) as is_featured,
-          COALESCE(prds.is_best_seller,0) as is_best_seller,
-          COALESCE(prds.is_free,0) as is_free,
-          COALESCE(prds.is_premium,0) as is_premium,
+        COALESCE(prds.is_featured,0) as is_featured,
+        COALESCE(prds.is_best_seller,0) as is_best_seller,
+        COALESCE(prds.is_free,0) as is_free,
+        COALESCE(prds.is_premium,0) as is_premium,
 
-          COALESCE(prds.ebook_price,0) as price,   
-          COALESCE(prds.ebook_discount_price,0) as discount_price,      
-          
-          COALESCE(prds.reorder_point,0) as reorder_point,  
-          COALESCE(prds.read_count,0) as read_count,  
+        COALESCE(prds.ebook_price,0) as price,   
+        COALESCE(prds.ebook_discount_price,0) as discount_price,      
+        
+        COALESCE(prds.reorder_point,0) as reorder_point,  
+        COALESCE(prds.read_count,0) as read_count,  
 
-          CONCAT(COALESCE(prds.name,''),' ', COALESCE(prds.author,''),'', COALESCE(prds.book_type,'') ,'', COALESCE(prds.subtitle,'')) as search_fields,
-
-          COALESCE((
-               SELECT 
-                  prod_img.path FROM 
-                      product_photos as prod_img                  
-                     WHERE prod_img.product_id = prod_det_cat.product_id   
-                        AND prod_img.is_primary = 1 
-                    LIMIT 1                                
-              )
-        ,'') as image_path,
-
-         COALESCE((
-             SELECT ROUND(avg(rating))
-                  FROM product_reviews as rev
-                  WHERE rev.product_id = prds.id
-                AND rev.status = 1 
-                LIMIT 1                                
-              )
-        ,0) as rating,
+        CONCAT(COALESCE(prds.name,''),' ', COALESCE(prds.author,''),'', COALESCE(prds.book_type,'') ,'', COALESCE(prds.subtitle,'')) as search_fields,
 
         COALESCE((
-               SELECT 
-                  promo.discount FROM 
-                        promos as promo                  
-                  INNER JOIN promo_products as promo_prods ON promo_prods.promo_id = promo.id  
-                       WHERE promo_prods.product_id = prod_det_cat.product_id  
-                       AND promo.applicable_product_type !='physical'
-                       AND promo.status = 'ACTIVE'                     
-                       AND promo_prods.deleted_at IS NULL 
-                       LIMIT 1                                
-              )
-        ,0) as promo_discount_percent,
+             SELECT 
+                prod_img.path FROM 
+                    product_photos as prod_img                  
+                   WHERE prod_img.product_id = prod_det_cat.product_id   
+                      AND prod_img.is_primary = 1 
+                  LIMIT 1                                
+            )
+      ,'') as image_path,
 
-        COALESCE((
-               SELECT 
-                   (prds.ebook_price - (promo.discount/100 * prds.ebook_price)) FROM 
-                        promos as promo                  
-                  INNER JOIN promo_products as promo_prods ON promo_prods.promo_id = promo.id  
-                       WHERE promo_prods.product_id = prod_det_cat.product_id  
-                       AND promo.applicable_product_type !='physical'
-                       AND promo.status = 'ACTIVE'                     
-                       AND promo_prods.deleted_at IS NULL LIMIT 1                                
-              )
-        ,0) as promo_discount_price,
+       COALESCE((
+           SELECT ROUND(avg(rating))
+                FROM product_reviews as rev
+                WHERE rev.product_id = prds.id
+              AND rev.status = 1 
+              LIMIT 1                                
+            )
+      ,0) as rating,
 
-         COALESCE((
-           SELECT 
-               bkmrk.chapter_no FROM 
-                    book_marks as bkmrk                  
-                INNER JOIN products as prods ON prods.id = bkmrk.product_id
-                     WHERE bkmrk.product_id = prds.id    
-                     AND bkmrk.customer_id=".$UserID." 
+      COALESCE((
+             SELECT 
+                promo.discount FROM 
+                      promos as promo                  
+                INNER JOIN promo_products as promo_prods ON promo_prods.promo_id = promo.id  
+                     WHERE promo_prods.product_id = prod_det_cat.product_id  
+                     AND promo.applicable_product_type !='physical'
+                     AND promo.status = 'ACTIVE'                     
+                     AND promo_prods.deleted_at IS NULL 
                      LIMIT 1                                
-          )
-      ,'') as chapter_no,
+            )
+      ,0) as promo_discount_percent,
 
-         COALESCE((
-               SELECT 
-                  cust_lib.product_id FROM 
-                customer_libraries as cust_lib                                    
-                      WHERE cust_lib.product_id = prds.id 
-                      AND cust_lib.user_id=".$UserID." LIMIT 1                                
-              )
-        ,0) as product_library_exist,
+      COALESCE((
+             SELECT 
+                 (prds.ebook_price - (promo.discount/100 * prds.ebook_price)) FROM 
+                      promos as promo                  
+                INNER JOIN promo_products as promo_prods ON promo_prods.promo_id = promo.id  
+                     WHERE promo_prods.product_id = prod_det_cat.product_id  
+                     AND promo.applicable_product_type !='physical'
+                     AND promo.status = 'ACTIVE'                     
+                     AND promo_prods.deleted_at IS NULL LIMIT 1                                
+            )
+      ,0) as promo_discount_price,
+
+       COALESCE((
+         SELECT 
+             bkmrk.chapter_no FROM 
+                  book_marks as bkmrk                  
+              INNER JOIN products as prods ON prods.id = bkmrk.product_id
+                   WHERE bkmrk.product_id = prds.id    
+                   AND bkmrk.customer_id= ?
+                   LIMIT 1                                
+        )
+    ,'') as chapter_no,
+
+       COALESCE((
+             SELECT 
+                cust_lib.product_id FROM 
+              customer_libraries as cust_lib                                    
+                    WHERE cust_lib.product_id = prds.id 
+                    AND cust_lib.user_id= ? LIMIT 1                                
+            )
+      ,0) as product_library_exist,
 
 
-          COALESCE(prds.status,'') as status        
+        COALESCE(prds.status,'') as status        
 
-        ");
+      ", [$UserID, $UserID]);
 
-       $query->where("prds.deleted_at","=",null);
-       $query->whereRaw("prod_det_cat.product_catalog_header_id =?",[$HeaderID]); 
-         
-     $list = $query->limit(10)->get();  // get temp 10
-                             
-     return $list;  
+     $query->where("prds.deleted_at","=",null);
+     $query->whereRaw("prod_det_cat.product_catalog_header_id =?",[$HeaderID]); 
+       
+   $list = $query->limit(10)->get();  // get temp 10
+                           
+   return $list;  
 
-  }
+}
  
   public function setBookAsReported($data){
 
