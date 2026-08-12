393f5ba4260c22597cc3691db9f212c5cfbf9040
ariz-kate <kate_paran@yahoo.com>
2026-08-12 17:56:51 +0800

    Update Book.php
    limit 10

--- app/Models/APIModels/Book.php
+++ app/Models/APIModels/Book.php
@@ -188,7 +188,7 @@ class Book extends Model
     }
 
     $query->orderBy("prds.created_at","ASC");    
-     $list = $query->limit(20)->get();  // get temp 10
+     $list = $query->limit(10)->get();  // get temp 10
                              
      return $list;             
            
@@ -716,7 +716,7 @@ class Book extends Model
     }  
     
 
-    $list = $query->limit(20)->get();  // get temp 10
+    $list = $query->limit(10)->get();  // get temp 10
                              
      return $list;             
            
