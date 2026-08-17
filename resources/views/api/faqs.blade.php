ad77cc58d58fc690dddf5686a96987464a5bfb67
ariz-kate <kate_paran@yahoo.com>
2026-08-17 17:29:18 +0800

    Update faqs.blade.php
    added image responsive css

--- resources/views/api/faqs.blade.php
+++ resources/views/api/faqs.blade.php
@@ -1,9 +1,49 @@
+<style>
+    .img-responsive {
+        display: block;
+        width: 100%;
+        max-width: 800px;
+        height: auto;
+        margin: 20px auto;
+    }
+</style>
 
-  @if(isset($Info))
-        @php($PageID = $Info->Page_ID)
-        @php($Content = $Info->faq)   
-  @endif
+@if(isset($Info))
+    @php
+        $PageID = $Info->Page_ID;
+        $Content = $Info->faq ?? '';
 
-  <div style="font-family: 'Poppins', sans-serif !important;color: #555;line-height: 1.5;font-size: 33px;padding:10px;"> 
-           {!! $Content !!}   
-  </div>
+        // Automatically add img-responsive class to all images
+        $Content = preg_replace_callback(
+            '/<img\b([^>]*)>/i',
+            function ($matches) {
+                $attributes = $matches[1];
+
+                // If image already has a class attribute
+                if (preg_match('/\bclass\s*=\s*["\']([^"\']*)["\']/i', $attributes)) {
+                    return preg_replace(
+                        '/\bclass\s*=\s*(["\'])(.*?)\1/i',
+                        'class=$1$2 img-responsive$1',
+                        $matches[0]
+                    );
+                }
+
+                // If image doesn't have a class
+                return '<img class="img-responsive"' . $attributes . '>';
+            },
+            $Content
+        );
+    @endphp
+@endif
+
+<div
+    style="
+        font-family: 'Poppins', sans-serif !important;
+        color: #555;
+        line-height: 1.5;
+        font-size: 33px;
+        padding: 10px;
+    "
+>
+    {!! $Content !!}
+</div>
