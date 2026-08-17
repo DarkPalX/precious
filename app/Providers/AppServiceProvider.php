03ddf68caed1b420613a5ed08d9011079ea14217
ariz-kate <kate_paran@yahoo.com>
2026-08-17 09:09:24 +0800

    Update AppServiceProvider.php
    apply rate limiter in middleware and throttle

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use  Illuminate\Support\Facades\Schema;
 
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Cache\RateLimiting\Limit;
+
 use Illuminate\Support\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
 
@@ -48,5 +51,20 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
         Schema::defaultStringLength(191);
+
+
+        // General API
+        // 60 requests per minute per authenticated user or IP
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(30)
+                ->by($request->user()?->id ?: $request->ip());
+        });
+
+        // Strict API
+        // 15 requests per minute per authenticated user or IP
+        RateLimiter::for('api-strict', function (Request $request) {
+            return Limit::perMinute(3)
+                ->by($request->user()?->id ?: $request->ip());
+        });
     }
 }
