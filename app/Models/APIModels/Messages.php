2288723ac5c45a67f80a97b399d78bca5da20de8
ariz-kate <kate_paran@yahoo.com>
2026-08-21 13:41:55 +0800

    Update Messages.php
    chance cache time

--- app/Models/APIModels/Messages.php
+++ app/Models/APIModels/Messages.php
@@ -25,47 +25,7 @@ use App\Models\APIModels\UserCustomer;
 class Messages extends Model
 {
   
-  // public function getMessageNotificationList($data){
-    
-  //   $UserID=$data['UserID'];
-    
-  //   $Status=$data['Status'];
-  //   $SearchText=$data['SearchText'];
-    
-  //   $Limit=$data['Limit'];
-  //   $PageNo=$data['PageNo'];
-    
-  //   $query = DB::table('message_notification as mssg_notif')
-  //     ->join('users as usrs', 'usrs.id', '=', 'mssg_notif.user_id')       
-    
-  //      ->selectraw("
-  //         mssg_notif.id as message_ID,
-          
-  //         COALESCE(mssg_notif.message_notification,'') as message_notification,        
-  //         DATE_FORMAT(mssg_notif.created_at,'%m/%d/%Y') as create_date_format,
-  //         COALESCE(mssg_notif.is_read,0) as is_read,   
-  //         COALESCE(mssg_notif.created_at,'') as created_at      
-          
-  //       ");    
-
-  //     $query->where("mssg_notif.user_id",'=',$UserID);                                
-  //     $query->where("mssg_notif.deleted_at",'=',null);
-
-
-  //   if($Limit > 0){
-  //     $query->limit($Limit);
-  //     $query->offset(($PageNo-1) * $Limit);
-  //   }
-   
-  //   $query->orderBy("mssg_notif.is_read","ASC");    
-  //   $query->orderBy("mssg_notif.created_at","DESC");    
-  //   $list = $query->get();
-                             
-  //    return $list;             
-           
-  // }
   
-
   public function getMessageNotificationList($data){
     
     $UserID=$data['UserID'];
@@ -78,7 +38,7 @@ class Messages extends Model
 
     $CacheKey = "message_notification_list_{$UserID}_{$Status}_{$SearchText}_{$Limit}_{$PageNo}";
 
-    $list = Cache::remember($CacheKey, now()->addSeconds(6), function () use ($UserID, $Status, $SearchText, $Limit, $PageNo) {
+    $list = Cache::remember($CacheKey, now()->addSeconds(4), function () use ($UserID, $Status, $SearchText, $Limit, $PageNo) {
 
         $query = DB::table('message_notification as mssg_notif')
           ->join('users as usrs', 'usrs.id', '=', 'mssg_notif.user_id')       
@@ -94,6 +54,7 @@ class Messages extends Model
             ");    
           $query->where("mssg_notif.user_id",'=',$UserID);                                
           $query->where("mssg_notif.deleted_at",'=',null);
+          
         if($Limit > 0){
           $query->limit($Limit);
           $query->offset(($PageNo-1) * $Limit);
